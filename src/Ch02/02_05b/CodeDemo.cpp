ffc8d18a891dfee1d5974c434add39ceee4caab0
dylwill-dev <dylwilldev@gmail.com>
2025-05-07 04:33:51 +0000

    Added code to demo file and tested alongside instruction.

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,30 @@
 
 #include <iostream>
 
+int a, b = 5; // Declare global variables a and b and initialize b to 5
+
 int main(){
     
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    unsigned int positive;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+
+    my_flag = true;
+
+    std::cout << "flag 2 = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+
+    positive = b - a;
+    std::cout << "b - a unsigned = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
+
+
     return 0;
 }
